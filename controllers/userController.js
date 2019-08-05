ad42ec12bf88464b6ca060d944d64c63416b6414
unknown <matim3k@gmail.com>
2019-08-05 23:18:33 +0100

    changing mysql connection method

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,32 @@ const helper = require('./helper.js');
 
 ///// ///// DB CONNETION
 // Establish connection with MySQL database using credentials from db.config file
-const connection = mysql.createConnection(dbconfig.connection);
+var connection
+function handleDisconnect() {
+    connection = mysql.createConnection(dbconfig.connection); // Recreate the connection, since
+    // the old one cannot be reused.
+
+    connection.connect(function (err) {              // The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+    // If you're also serving http, display a 503 error.
+    connection.on('error', function (err) {
+        console.log('db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
+
+
 
 ///// ///// END-POINTS FUNCTIONS
 exports.registerUser = async (req, res) => {
@@ -18,7 +43,7 @@ exports.registerUser = async (req, res) => {
     const email = body.email;
     const password = body.password;
     // const hashed_password = await bcrypt.hash(password, 10)
-    const hashed_password = bcrypt.hashSync(password,10);
+    const hashed_password = bcrypt.hashSync(password, 10);
     console.log("email: " + email + "\npassword: " + password + "\nhashed_password: " + hashed_password)
     const sql = 'SELECT * FROM user WHERE email = ?';
 
@@ -69,7 +94,7 @@ exports.loginUser = async (req, res) => {
         try {
             if (await bcrypt.compare(req.body.password, user.password)) {
                 console.log("SUCCESS")
-                return res.status(200).send(""+user.userID)
+                return res.status(200).send("" + user.userID)
             }
             else {
                 console.log("FAIL")
@@ -183,7 +208,7 @@ exports.updateUserPassword = async (req, res) => {
     const user_id = req.params.uid
     const old_password = req.body.oldpass
     const new_password = req.body.newpass
-    const hashed_password = bcrypt.hashSync(new_password,10);
+    const hashed_password = bcrypt.hashSync(new_password, 10);
 
     const sql =
         'SELECT * FROM user WHERE userID = ?';
